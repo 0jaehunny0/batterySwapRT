cff01cf78e8ca472045905aaba67d288350da29f
Jaeheon Kwak <0jaehunny0@gmail.com>
2024-05-19 15:07:22 +0900

    advanced analysis

--- analysis.py
+++ analysis.py
@@ -23,13 +23,37 @@ def analysisCG(taskSet, params, batterySet):
 
     T = taskSet[:,_T]
     C = taskSet[:,_CG]
+    D = taskSet[:,_VD]
     NCG = numc
 
+    """ first round Rx <- Cx, Ry <- Dy """
+    prevR = taskSet[:, _RCG]
+    myNBL = np.floor((np.fmax(0, C - T)) / T)
+    myBL = np.fmin(C, ((np.fmax(0, C - T)) % T))
+    NBL = np.floor((np.fmax(0, prevR - T)) / T)
+    BL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+    newRList = []
+    for idx in range(numt):
+        first = C[idx]
+        second = np.ceil ((myNBL[idx] * C[idx] + myBL[idx]) / NCG)        
+        
+        fast = sum(C) + sum(NBL*C) + sum(BL) - C[idx] - NBL[idx] * C[idx] - BL[idx]
+        thrid = np.ceil(fast / NCG)
+
+        newR = np.ceil( first + second + thrid )
+        newRList.append(newR)
+    newRList = np.array(newRList)
+    taskSet[:, _RCG] = newRList
+    
+    """ Loop"""
     while True:
 
         prevR = taskSet[:, _RCG]
-        NBL = np.floor((np.fmax(0, prevR - T)) / T)
-        BL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+        prevRy = np.fmin(D, prevR)
+        myNBL = np.floor((np.fmax(0, prevR - T)) / T)
+        myBL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+        NBL = np.floor((np.fmax(0, prevRy - T)) / T)
+        BL = np.fmin(C, ((np.fmax(0, prevRy - T)) % T))
 
         newRList = []
         for idx in range(numt):
@@ -46,14 +70,14 @@ def analysisCG(taskSet, params, batterySet):
 
         if sum(prevR == newRList) == numt:
 
-            if sum(taskSet[:, _RCG] >= taskSet[:,_VD]) >= 1:
+            if sum(taskSet[:, _RCG] + taskSet[:, _RSW] >= taskSet[:,_VD]) >= 1:
                 return [-1]
-            
+            # print(taskSet[:, _RCG])
             return taskSet # schedulable, conversed
 
         if sum(prevR <= newRList) == numt:
 
-            if sum(taskSet[:, _RCG] >= taskSet[:,_VD]) >= 1:
+            if sum(taskSet[:, _RCG] + taskSet[:, _RSW] >= taskSet[:,_VD]) >= 1:
                 return [-1]
             
             return taskSet # schedulable, conversed
@@ -93,13 +117,37 @@ def analysisSW(taskSet, params, batterySet):
 
     T = taskSet[:,_T]
     C = taskSet[:,_C]
+    D = taskSet[:,_D]
     NSW = nump
 
+    """ first round Rx <- Cx, Ry <- Dy """
+    prevR = taskSet[:, _RSW]
+    myNBL = np.floor((np.fmax(0, C - T)) / T)
+    myBL = np.fmin(C, ((np.fmax(0, C - T)) % T))
+    NBL = np.floor((np.fmax(0, prevR - T)) / T)
+    BL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+    newRList = []
+    for idx in range(numt):
+        first = C[idx]
+        second = np.ceil ((myNBL[idx] * C[idx] + myBL[idx]) / NSW)        
+        
+        fast = sum(C) + sum(NBL*C) + sum(BL) - C[idx] - NBL[idx] * C[idx] - BL[idx]
+        thrid = np.ceil(fast / NSW)
+
+        newR = np.ceil( first + second + thrid )
+        newRList.append(newR)
+    newRList = np.array(newRList)
+    taskSet[:, _RSW] = newRList
+    
+    """ Loop"""
     while True:
 
         prevR = taskSet[:, _RSW]
-        NBL = np.floor((np.fmax(0, prevR - T)) / T)
-        BL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+        prevRy = np.fmin(D, prevR)
+        myNBL = np.floor((np.fmax(0, prevR - T)) / T)
+        myBL = np.fmin(C, ((np.fmax(0, prevR - T)) % T))
+        NBL = np.floor((np.fmax(0, prevRy - T)) / T)
+        BL = np.fmin(C, ((np.fmax(0, prevRy - T)) % T))
 
         newRList = []
         for idx in range(numt):
@@ -116,6 +164,7 @@ def analysisSW(taskSet, params, batterySet):
         if sum(prevR == newRList) == numt:
             if sum(taskSet[:, _RSW] >= taskSet[:,_D]) >= 1:
                 return [-1]
+            # print(taskSet[:, _RSW])
             return taskSet # schedulable, conversed
 
         if sum(prevR <= newRList) == numt:
@@ -127,3 +176,172 @@ def analysisSW(taskSet, params, batterySet):
             # return [-1] # unsched
         
         taskSet[:, _RSW] = newRList
+
+
+def NEWanalysisSW(taskSet, params, batterySet):
+
+    sUtil, cUtil, numt, nump, numc, NUMS = params
+
+    # init R_x(i) = D_x
+    taskSet = np.hstack((taskSet, np.array([taskSet[:, _D]]).T))
+
+    T = taskSet[:,_T]
+    C = taskSet[:,_C]
+    D = taskSet[:,_D]
+    NSW = nump
+
+    """ first round Rx <- Cx, Ry <- Dy """
+    prevR = taskSet[:, _RSW]
+    myBL = []
+    BL = []
+    for idx in range(numt):
+        onemyBL = 0
+        oneBL = 0
+        if C[idx] <= T[idx]:
+            for a in range(1, int(np.floor(C[idx]/T[idx])) + 1):
+                onemyBL += np.min([C[idx], C[idx] - a * T[idx]])
+        if D[idx] <= T[idx]:
+            for a in range(1, int(np.floor(D[idx]/T[idx])) + 1):
+                oneBL += np.min([C[idx], D[idx] - a * T[idx]])
+        myBL.append(onemyBL)
+        BL.append(oneBL)
+    myBL = np.array(myBL)
+    BL = np.array(BL)
+    newRList = []
+    for idx in range(numt):
+        first = C[idx]
+        second = myBL[idx]
+        thrid = sum(C) + sum(BL) - C[idx] - BL[idx]
+        newR = np.ceil( first + (second + thrid)/NSW)
+        newRList.append(newR)
+    newRList = np.array(newRList)
+    taskSet[:, _RSW] = newRList
+    
+    """ Loop"""
+    while True:
+
+        prevR = taskSet[:, _RSW]
+        prevRy = np.fmin(D, prevR)
+        myBL = []
+        BL = []
+        for idx in range(numt):
+            onemyBL = 0
+            oneBL = 0
+            if prevR[idx] <= T[idx]:
+                for a in range(1, int(np.floor(prevR[idx]/T[idx])) + 1):
+                    onemyBL += np.min([C[idx], prevR[idx] - a * T[idx]])
+            if prevRy[idx] <= T[idx]:
+                for a in range(1, int(np.floor(prevRy[idx]/T[idx])) + 1):
+                    oneBL += np.min([C[idx], prevRy[idx] - a * T[idx]])
+            myBL.append(onemyBL)
+            BL.append(oneBL)
+        myBL = np.array(myBL)
+        BL = np.array(BL)
+        newRList = []
+        for idx in range(numt):
+            first = C[idx]
+            second = myBL[idx]
+            thrid = sum(C) + sum(BL) - C[idx] - BL[idx]
+            newR = np.ceil( first + (second + thrid)/NSW)
+            newRList.append(newR)
+        newRList = np.array(newRList)
+
+        if sum(prevR == newRList) == numt:
+            if sum(taskSet[:, _RSW] >= taskSet[:,_D]) >= 1:
+                return [-1]
+            # print(taskSet[:, _RSW])
+            return taskSet # schedulable, conversed
+
+        if sum(prevR <= newRList) == numt:
+
+            if sum(taskSet[:, _RSW] >= taskSet[:,_D]) >= 1:
+                return [-1]
+            return taskSet # schedulable, conversed
+
+            # return [-1] # unsched
+        
+        taskSet[:, _RSW] = newRList
+
+def NEWanalysisCG(taskSet, params, batterySet):
+
+    sUtil, cUtil, numt, nump, numc, NUMS = params
+
+    # init R_x(i) = D_x
+    taskSet = np.hstack((taskSet, np.array([taskSet[:, _VD]]).T))
+
+    T = taskSet[:,_T]
+    C = taskSet[:,_CG]
+    D = taskSet[:,_VD]
+    NCG = numc
+
+    """ first round Rx <- Cx, Ry <- Dy """
+    prevR = taskSet[:, _RCG]
+    myBL = []
+    BL = []
+    for idx in range(numt):
+        onemyBL = 0
+        oneBL = 0
+        if C[idx] <= T[idx]:
+            for a in range(1, int(np.floor(C[idx]/T[idx])) + 1):
+                onemyBL += np.min([C[idx], C[idx] - a * T[idx]])
+        if D[idx] <= T[idx]:
+            for a in range(1, int(np.floor(D[idx]/T[idx])) + 1):
+                oneBL += np.min([C[idx], D[idx] - a * T[idx]])
+        myBL.append(onemyBL)
+        BL.append(oneBL)
+    myBL = np.array(myBL)
+    BL = np.array(BL)
+    newRList = []
+    for idx in range(numt):
+        first = C[idx]
+        second = myBL[idx]
+        thrid = sum(C) + sum(BL) - C[idx] - BL[idx]
+        newR = np.ceil( first + (second + thrid)/NCG)
+        newRList.append(newR)
+    newRList = np.array(newRList)
+    taskSet[:, _RCG] = newRList
+    
+    """ Loop"""
+    while True:
+
+        prevR = taskSet[:, _RCG]
+        prevRy = np.fmin(D, prevR)
+        myBL = []
+        BL = []
+        for idx in range(numt):
+            onemyBL = 0
+            oneBL = 0
+            if prevR[idx] <= T[idx]:
+                for a in range(1, int(np.floor(prevR[idx]/T[idx])) + 1):
+                    onemyBL += np.min([C[idx], prevR[idx] - a * T[idx]])
+            if prevRy[idx] <= T[idx]:
+                for a in range(1, int(np.floor(prevRy[idx]/T[idx])) + 1):
+                    oneBL += np.min([C[idx], prevRy[idx] - a * T[idx]])
+            myBL.append(onemyBL)
+            BL.append(oneBL)
+        myBL = np.array(myBL)
+        BL = np.array(BL)
+        newRList = []
+        for idx in range(numt):
+            first = C[idx]
+            second = myBL[idx]
+            thrid = sum(C) + sum(BL) - C[idx] - BL[idx]
+            newR = np.ceil( first + (second + thrid)/NCG)
+            newRList.append(newR)
+        newRList = np.array(newRList)
+
+        if sum(prevR == newRList) == numt:
+            if sum(taskSet[:, _RCG] + taskSet[:, _RSW] >= taskSet[:,_VD]) >= 1:
+                return [-1]
+            # print(taskSet[:, _RCG])
+            return taskSet # schedulable, conversed
+
+        if sum(prevR <= newRList) == numt:
+
+            if sum(taskSet[:, _RCG] + taskSet[:, _RSW] >= taskSet[:,_VD]) >= 1:
+                return [-1]
+            return taskSet # schedulable, conversed
+
+            # return [-1] # unsched
+        
+        taskSet[:, _RCG] = newRList
